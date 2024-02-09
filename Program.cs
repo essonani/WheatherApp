275af1614660c04abd4e4f6ef77c68bd8a696af1
essonani tchakpalla <essonani@hotmail.com>
2024-02-08 22:12:31 -0600

    swe

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
-﻿namespace WheatherApp
+﻿using Newtonsoft.Json.Linq;
+
+namespace WheatherApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string key = File.ReadAllText("appsettings.json");
+            string APIkey=JObject.Parse(key).GetValue("APIKey").ToString();
+            Console.WriteLine("enter your zip code");
+            var zipCode=Console.ReadLine();
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIkey}";
+            Console.WriteLine("temperature: " + WheatherMap.GetTemp(apiCall));
         }
     }
 }
